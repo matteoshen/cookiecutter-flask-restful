b563568e4098a3f7056df80135d6a6e8a77793bf
lijun.S <lijun@MacbookPro>
2020-01-31 10:45:42 +0800

    add username_cn in user

--- restful_api/myapi/manage.py
+++ restful_api/myapi/manage.py
@@ -21,7 +21,13 @@ def init():
     from myapi.models import User
 
     click.echo("create user")
-    user = User(username="admin", email="admin@mail.com", password="admin", active=True)
+    user = User(
+        username="admin",
+        username_cn="系统管理员",
+        email="admin@mail.com",
+        password="admin",
+        active=True,
+    )
     db.session.add(user)
     db.session.commit()
     click.echo("created user admin")
