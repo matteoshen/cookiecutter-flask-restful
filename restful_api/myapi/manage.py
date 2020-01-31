d3d8fb6ac4fd9cdbc9b47f2840f23b6719aaaa53
lijun.S <lijun@MacbookPro>
2020-01-31 16:50:49 +0800

    normal user and role in init

--- restful_api/myapi/manage.py
+++ restful_api/myapi/manage.py
@@ -15,13 +15,13 @@ def cli():
 
 @cli.command("init")
 def init():
-    """Create a new admin user and a new admins role
+    """Create a new admin user and new roles
     """
     from myapi.extensions import db
     from myapi.models import User, Role
 
     click.echo("create user")
-    user = User(
+    user_admin = User(
         username="admin",
         username_cn="系统管理员",
         email="admin@mail.com",
@@ -29,15 +29,27 @@ def init():
         role_id=1,
         active=True,
     )
-    db.session.add(user)
-    db.session.commit()
-    click.echo("created user admin")
+    db.session.add(user_admin)
+    user_test = User(
+        username="normal",
+        username_cn="普通用户",
+        email="normal@normal.com",
+        password="normal",
+        role_id=2,
+        active=True,
+    )
+    db.session.add(user_admin)
+    db.session.add(user_test)
+    db.sesson.commit()
+    click.echo("created user admin, normal")
 
     click.echo("create role")
-    role = Role(role_name="admins", role_name_cn="系统管理员角色",)
-    db.session.add(role)
+    role_admins = Role(role_name="admins", role_name_cn="系统管理员角色",)
+    role_normals = Role(role_name="normals", role_name_cn="普通用户角色",)
+    db.session.add(role_admins)
+    db.session.add(role_normals)
     db.session.commit()
-    click.echo("created role admins")
+    click.echo("created role admins, normals")
 
 
 if __name__ == "__main__":
